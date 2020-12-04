bd5d5e9f29a87c89c115a93d497211178783bbbb
kocmeek <melek.koc.066@gmail.com>
2020-12-04 07:25:07 +0300

    Update Urun3ApiController.cs

--- WebApi/Urun3ApiController.cs
+++ WebApi/Urun3ApiController.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 
-namespace otoparkcogencomtr.Controllers
+namespace otoparkyonetim.Controllers
 {
     public class Urun3ApiController : ApiController
     {
