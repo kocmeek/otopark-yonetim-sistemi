682ea9faad2fce1f0f4b17f6711480a8c9e56020
kocmeek <melek.koc.066@gmail.com>
2020-12-04 07:16:47 +0300

    Update KullaniciTipiYetkiController.cs

--- Kullanıcıİslemleri/KullaniciTipiYetkiController.cs
+++ Kullanıcıİslemleri/KullaniciTipiYetkiController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web.Mvc;
 using X.PagedList;
 
-namespace otoparkcogencomtr.Controllers
+namespace otoparkyonetim.Controllers
 {
     public class KullaniciTipiYetkiController : Controller
     {
